086d1c4702bc5e9f07ab30e0ac64941d49ec9de2
Felipe Oliveira <oliv.felipesantos@gmail.com>
2025-02-20 21:29:47 +0100

    Added URL field in password entries

--- passafe/password_vault/static/password_vault/js/copyUsername.js
+++ passafe/password_vault/static/password_vault/js/copyUsername.js
@@ -1,6 +1,6 @@
+// Function to copy username to clipboard
 function copyUsername(username) {
     navigator.clipboard.writeText(username).then(() => {
-        // Optionally, display a success message (e.g., using a toast or tooltip)
         console.log("Username copied: " + username);
     }).catch(err => {
         console.error("Failed to copy username: ", err);
